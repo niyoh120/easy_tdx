67d9963f207cce33b4fb855632b6c6b58f19a08f
GitHub <action@github.com>
2026-06-12 19:43:54 +0800

    feat(factor): add Factor base class and registry

---
+++ src/easy_tdx/factor/__init__.py
@@ -0,0 +1,6 @@
+# src/easy_tdx/factor/__init__.py
+"""因子研究模块。"""
+
+from easy_tdx.factor.base import FACTORY_REGISTRY, Factor, register_factor
+
+__all__ = ["Factor", "register_factor", "FACTORY_REGISTRY"]
